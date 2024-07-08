a4e72e02fc00d0f9608410b3e77745b8ec887684
annikacode <annikahenry5@gmail.com>
2024-07-08 21:19:14 +0200

    added countdown clock

---
+++ script.js
@@ -0,0 +1,23 @@
+function updateCountdown() {
+    const targetDate = new Date("November 1, 2024 00:00:00").getTime();
+    const now = new Date().getTime();
+    const difference = targetDate - now;
+
+    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerText = days;
+    document.getElementById("hours").innerText = hours;
+    document.getElementById("minutes").innerText = minutes;
+    document.getElementById("seconds").innerText = seconds;
+
+    if (difference < 0) {
+        clearInterval(countdownInterval);
+        document.getElementById("countdown").innerText = "The Wedding Day is Here!";
+    }
+}
+
+const countdownInterval = setInterval(updateCountdown, 1000);
+updateCountdown();
